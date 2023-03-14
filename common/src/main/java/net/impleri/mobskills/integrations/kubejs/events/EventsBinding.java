204c2bd6bbc991a3ba966f561ce49010e37c11b0
Christopher Roussel <christopher@impleri.net>
2023-03-14 15:48:36 -0500

    feature: Backport to 1.18.2

--- common/src/main/java/net/impleri/mobskills/integrations/kubejs/events/EventsBinding.java
+++ common/src/main/java/net/impleri/mobskills/integrations/kubejs/events/EventsBinding.java
@@ -1,10 +1,5 @@
 package net.impleri.mobskills.integrations.kubejs.events;
 
-import dev.latvian.mods.kubejs.event.EventGroup;
-import dev.latvian.mods.kubejs.event.EventHandler;
-
 public abstract class EventsBinding {
-    public static final EventGroup GROUP = EventGroup.of("MobSkillEvents");
-
-    public static final EventHandler RESTRICTIONS = GROUP.server("register", () -> RestrictionsRegistrationEventJS.class);
+    public static final String RESTRICTIONS = "mobSkills.register";
 }
