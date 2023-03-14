2dd40a574d8dbb6f85685760edd863b22c41c815
Christopher Roussel <christopher@impleri.net>
2023-03-14 15:48:36 -0500

    feature: Backport to 1.18.2

--- common/src/main/java/net/impleri/mobskills/integrations/kubejs/MobSkillsPlugin.java
+++ common/src/main/java/net/impleri/mobskills/integrations/kubejs/MobSkillsPlugin.java
@@ -1,16 +1,12 @@
 package net.impleri.mobskills.integrations.kubejs;
 
 import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.ScriptType;
 import net.impleri.mobskills.integrations.kubejs.events.EventsBinding;
 import net.impleri.mobskills.integrations.kubejs.events.RestrictionsRegistrationEventJS;
 
 public class MobSkillsPlugin extends KubeJSPlugin {
-    @Override
-    public void registerEvents() {
-        EventsBinding.GROUP.register();
-    }
-
     public static void onStartup() {
-        EventsBinding.RESTRICTIONS.post(new RestrictionsRegistrationEventJS());
+        new RestrictionsRegistrationEventJS().post(ScriptType.SERVER, EventsBinding.RESTRICTIONS);
     }
 }
