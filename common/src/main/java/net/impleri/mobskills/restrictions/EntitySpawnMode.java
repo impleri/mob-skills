c7b22dd4f7237ed516362251b5b00fc8d4aa1874
Christopher Roussel <christopher@impleri.net>
2023-03-21 22:32:32 -0500

    feature: Spawn restrictions based on the dimension or biome

--- common/src/main/java/net/impleri/mobskills/restrictions/EntitySpawnMode.java
+++ common/src/main/java/net/impleri/mobskills/restrictions/EntitySpawnMode.java
@@ -8,49 +8,45 @@ public enum EntitySpawnMode {
 
     /**
      * Allow the spawn if any player matches the predicate.
-     * If 1 matches, yes === If none match, no
      */
     ALLOW_IF_ANY_MATCH,
 
     /**
      * Allow the spawn if all players match the predicate.
-     * If all match, yes === if 1 doesn't match, no
      */
     ALLOW_IF_ALL_MATCH,
 
     /**
-     * Allow the spawn if any player doesn't match the predicate.
-     * if 1 doesn't match, yes === if all match, no
+     * Allow the spawn if all players do not match the predicate.
      */
     ALLOW_UNLESS_ANY_MATCH,
 
     /**
-     * Allow the spawn if all players match the predicate.
-     * if all doesn't match, yes === if 1 matches, no
+     * Allow the spawn if any player does not match the predicate.
      */
     ALLOW_UNLESS_ALL_MATCH,
 
     /**
      * Deny the spawn if any player match the predicate.
-     * if 1 matches, no === If all doesn't match, yes
+     * Allow the spawn if all players do not match the predicate.
      */
     DENY_IF_ANY_MATCH,
 
     /**
      * Deny the spawn if all players match the predicate.
-     * if all match, no === if 1 doesn't match, yes
+     * Allow the spawn if any player does not match the predicate.
      */
     DENY_IF_ALL_MATCH,
 
     /**
      * Deny the spawn if no player match the predicate.
-     * if none match, no === If 1 matches, yes
+     * Allow the spawn if any player matches the predicate.
      */
     DENY_UNLESS_ANY_MATCH,
 
     /**
-     * Deny the spawn if any player does not match the predicate.
-     * if 1 doesn't match, no === If all matches, yes
+     * Deny the spawn if all players do not match the predicate.
+     * Allow the spawn if all players match the predicate.
      */
     DENY_UNLESS_ALL_MATCH,
 
